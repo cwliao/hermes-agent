Carrie111998/Hermes

fix(gateway): isolate post-turn loop failures

gateway/run.py
@@ -20985,22 +20985,29 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                 source,
                 touch_activity=not is_internal,
             )
-        except Exception:
+        except Exception as exc:
+            logger.debug("post-turn session resolution failed: %s", exc)
             return
 
         # Empty interrupted/errored responses must not drive /goal, but an
         # in-flight /loop tick still needs to be released and rescheduled.
         if final_text.strip():
-            await self._post_turn_goal_continuation(
+            try:
+                await self._post_turn_goal_continuation(
+                    session_entry=session_entry,
+                    source=source,
+                    final_response=final_text,
+                )
+            except Exception as exc:
+                logger.debug("goal continuation hook failed: %s", exc)
+        try:
+            await self._post_turn_loop_completion(
                 session_entry=session_entry,
                 source=source,
                 final_response=final_text,
             )
-        await self._post_turn_loop_completion(
-            session_entry=session_entry,
-            source=source,
-            final_response=final_text,
-        )
+        except Exception as exc:
+            logger.debug("loop completion hook failed: %s", exc)
 
 
 

cwliao/hermes-agent

gateway/run.py
@@ -10536,18 +10536,24 @@ class GatewayRunner(GatewayAuthorizationMixin, GatewayKanbanWatchersMixin, Gatew
                     session_entry = None
                 if session_entry is not None:
                     if _final_text.strip():
-                        await self._post_turn_goal_continuation(
+                        try:
+                            await self._post_turn_goal_continuation(
+                                session_entry=session_entry,
+                                source=source,
+                                final_response=_final_text,
+                            )
+                        except Exception as _goal_exc:
+                            logger.debug("goal continuation hook failed: %s", _goal_exc)
+                    try:
+                        await self._post_turn_loop_completion(
                             session_entry=session_entry,
                             source=source,
                             final_response=_final_text,
                         )
-                    await self._post_turn_loop_completion(
-                        session_entry=session_entry,
-                        source=source,
-                        final_response=_final_text,
-                    )
+                    except Exception as _loop_exc:
+                        logger.debug("loop completion hook failed: %s", _loop_exc)
             except Exception as _goal_exc:
-                logger.debug("goal continuation hook failed: %s", _goal_exc)
+                logger.debug("post-turn hook failed: %s", _goal_exc)
             return _agent_result
         finally:
             # MoA one-shot restore must run on EVERY exit path, not just